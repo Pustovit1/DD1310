c9282f6506a9d4a42d4c93abb3f71967bab9c1f7
Taras Pustovit <pustovittaras48@gmail.com>
2025-10-28 16:24:54 +0100

    Laboration 1. Har gjort ett program som frågar användare om dens namn och ålder, efter att ha fått ålder i form av antalet år omvanldar den antalet år till antalet dagar, timmar, minuter.

---
+++ vecka1/labb1.py
@@ -0,0 +1,24 @@
+# Laboration 1
+# DD1310
+# Taras Pustovit CENMI1
+# Detta är en program som frågar användare om dens namn och ålder, efter att ha fått ålder i form av antalet år omvanldar den antalet år till antalet dagar, timmar, minuter. 
+
+print("Hej! Vad heter du?")
+name = input()
+print(f"Trevligt att träffas {name}! Mitt namn är Taras. Hur gammal är du?")
+while True:
+    try:
+        age = int(input())
+        break   
+    except:
+        print("Ålder måste anges som ett tal utan bokstäver.")
+
+days = age*365.25
+hours = days*24
+minutes = hours*60
+
+if age < 18:
+    print(f"Du är inte vuxen än men {age} år är också en långt tid! {age} år motsvarar {days} dagar, {hours} timmar, {minutes} minuter!")
+else:
+    print(f"Du är vuxen människa, {age} år är en lång tid! {age} år motsvarar {days} dagar, {hours} timmar, {minutes} minuter!")
+print(f"Tyvärr har jag inte mer tid för att prata med dig. Hejdå {name}, tack för det trevliga samtalet!")
